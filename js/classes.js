7da9a7c43572c77d5d424fe718318530c3067c9d
Jordi Martinez <jordiupc87@gmail.com>
2022-09-23 17:57:31 +0200

    Basic Structure

---
+++ js/classes.js
@@ -0,0 +1,10 @@
+class MainChar {
+  constructor(level, hp, attack, isGodModeEnabled) {
+    this.level = level;
+    this.hp = hp;
+    this.attack = attack;
+    this.isGodModeEnabled = isGodModeEnabled;
+  }
+  baseAttack() {}
+  blink(distance) {}
+}
